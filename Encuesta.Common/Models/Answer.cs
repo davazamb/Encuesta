a50402cfdd432a2b444f5765edf6ce2a5d990cb0
David Ali Zambrano Burgos <davazamb10@outlook.com>
2018-08-12 13:35:47 -0300

    Creada conexion y modelos para el proyecto Domain y common

---
+++ Encuesta.Common/Models/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encuesta.Common.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int AnswerId { get; set; }
+        public int PollId { get; set; }
+        public string AnswerString { get; set; }
+        public int Votes { get; set; }
+        public DateTime DateAnswer { get; set; }
+
+    }
+}
---
+++ Encuesta.Common/Models/Poll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encuesta.Common.Models
+{
+    public class Poll
+    {
+        [Key]
+        public int PollId { get; set; }
+        public string Question { get; set; }
+        public string User { get; set; }
+        public string IsAvailable { get; set; }
+        public DateTime DatePoll { get; set; }
+    }
+}
