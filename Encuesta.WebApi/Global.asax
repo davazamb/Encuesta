be83ad914062db30311a8a62fb397d16d8bb39c2
David Ali Zambrano Burgos <davazamb10@outlook.com>
2018-08-12 13:11:51 -0300

    Creado proyectos y divicion de dependencias, webapi y app xamrin android, ios, uwp

---
+++ Encuesta.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Encuesta.WebApi.MvcApplication" Language="C#" %>
---
+++ Encuesta.WebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Encuesta.WebApi.Startup))]
+namespace Encuesta.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
